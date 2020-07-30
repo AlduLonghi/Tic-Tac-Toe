0c271365ed7c4e92c8a4cf26501d1b267d18e77a
Aldana Abril Longhi <aldulonghi@gmail.com>
2020-07-30 06:06:35 -0300

    add initial game class with ifno about Tic-Tac-Toe

--- bin/main.rb
+++ bin/main.rb
@@ -1,2 +1,91 @@
 #!/usr/bin/env ruby
-puts 'Hello World!'
+
+=begin
+ Tic-Tac-Toe game consists of:
+
+ -Start the game.
+
+ -Get players' name.
+
+ -Assign a symbol for each player.
+
+ -Start the round.
+
+ -Display board.
+
+ -Get player's input for the cell he chooses.
+
+ -Play the logic for the different input possibilities.
+
+ -Check the score.
+
+ -Announce the winner or if it is a tie.
+
+ -Give the possibility of continue or quit.
+=end
+
+#First class is for giving info about the game and getting players' info
+
+class Start
+
+    def initialize 
+      puts ' '
+      puts '************************************************************************************************************'
+      puts ' '
+      puts '                                     WELCOME TO THE GAME!'
+      puts ' '
+      puts '************************************************************************************************************'
+      puts ' '
+      puts ' '
+      puts '                       RULES FOR TIC-TAC-TOE:'
+      puts ' '
+      puts '1. The game is played on a grid that\'s 3 squares by 3 squares.'
+      puts ' '
+      puts '2. You are assigned a symbol to play with and your friend is assigned another one.'
+      puts ' '
+      puts '3. The first player to get 3 of her marks in a row (up, down, across, or diagonally) is the winner.'
+      puts ' '
+      puts '4. When all 9 squares are full, the game is over. If no player has 3 marks in a row, the game ends in a tie.'
+      puts ' '
+      puts ' '
+      players
+      #Starting with the game logic
+      play = Game.new
+    end
+
+    def players 
+      puts ' '
+      puts 'PLEASE ENTER PLAYER NAME 1: '
+      $player1 = gets.chomp()
+      puts ' '
+      puts "Hello #{$player1}! Your symbol is: ♥"
+      puts ' '
+      puts 'PLEASE ENTER PLAYER NAME 2: '
+      $player2 = gets.chomp()
+      puts ' '
+      puts "Hello #{$player2}! Your symbol is: ▩"
+      puts ' '
+      puts "Lets begin with #{$player1}!"
+      puts ' '
+    end
+
+end
+
+class Game 
+    $a = "a"
+    $b = "b"
+    $c = "c"
+    $d = "d"
+    $e = "e"
+    $f = "f"
+    $g = "g"
+    $h = "h"
+    $i = "i"
+    $counter = ["a", "b", "c", "d", "e", "f", "g", "h", "i"]
+
+    def initialize
+
+
+
+
+game = Start.new
